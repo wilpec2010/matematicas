aa20a80f6ee3b587c587fbd7343afff2e00d9936
wilpec2023 <wilpec2017@gmail.com>
2023-01-16 20:02:27 -0500

    ültimo cambio

--- perimetros.py
+++ perimetros.py
@@ -9,7 +9,7 @@ def perRectangulo ():
     return print( "El area del Rectangulo es: " , 2 * base + 2* altura) # 16 de Julio de 2023
 perRectangulo() # 31 de julio de 2023
 
-pi= 3,1416
+pi= 3,141617000000000000000
 
 def perCirculo ():
     radio= float(input("ingrese el valor de la radio: "))
