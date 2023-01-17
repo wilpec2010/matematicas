a15c5075709f4eda9057fc819753d78fde6732c8
wilpec2023 <wilpec2017@gmail.com>
2023-01-16 19:41:41 -0500

    El segundo de la rama Nueva

---
+++ perimetros.py
@@ -0,0 +1,19 @@
+def perCuadrado ():
+    lado= float(input("ingrese el valor de perimetro : "))
+    return print( "El area del cuadrado es: " , lado *4) # 16 de Julio de 2023
+perCuadrado() # 30 de julio de 2023
+
+def perRectangulo ():
+    base= float(input("ingrese el valor de la base: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , 2 * base + 2* altura) # 16 de Julio de 2023
+perRectangulo() # 31 de julio de 2023
+
+pi= 3,1416
+
+def perCirculo ():
+    radio= float(input("ingrese el valor de la radio: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , 2+ pi *radio) # 16 de Julio de 2023
+perCirculo() # 31 de julio de 2023
+
